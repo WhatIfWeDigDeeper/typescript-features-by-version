7970a0ae2f8483b042429a4f84d3fbc78902d379
WhatIfWeDigDeeper <1247548+WhatIfWeDigDeeper@users.noreply.github.com>
2020-09-09 07:54:59 -0500

    3.0, readme addition

--- README.md
+++ README.md
@@ -1,53 +1,61 @@
-# typescript-features-by-version
+# Typescript Features by Version
 
-Test specs to demonstrate each major and minor feature of Typescript based on release notes from the Typescript team.
+Test specs to demonstrate each major and minor feature of Typescript based on release notes from the Typescript team. You may use debug launch configurations of
+
+- Jest Tests = debug all unit Tests
+- Jest Active Unit Test = debug the current test file
 
 ## 4.0
 
-- [test](src/4.0.spec.ts)
+- [test spec](src/4.0.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-0.html)
 
-- ## 3.9
+## 3.9
 
-- [test](src/3.9.spec.ts)
+- [test spec](src/3.9.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-9.html)
 
 ## 3.8
 
-- [test](src/3.8.spec.ts)
+- [test spec](src/3.8.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html)
 
 ## 3.7
 
-- [test](src/3.7.spec.ts)
+- [test spec](src/3.7.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html)
 
 ## 3.6
 
-- [test](src/3.6.spec.ts)
+- [test spec](src/3.6.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-6.html)
 
 ## 3.5
 
-- [test](src/3.5.spec.ts)
+- [test spec](src/3.5.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-5.html)
 
 ## 3.4
 
-- [test](src/3.4.spec.ts)
+- [test spec](src/3.4.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html)
 
 ## 3.3
 
-- [test](src/3.3.spec.ts)
+- [test spec](src/3.3.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-3.html)
 
 ## 3.2
 
-- [test](src/3.2.spec.ts)
+- [test spec](src/3.2.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-2.html)
 
 ## 3.1
 
-- [test](src/3.1.spec.ts)
+- [test spec](src/3.1.spec.ts)
 - [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-1.html)
+
+## 3.0
+
+- [test spec](src/3.0.spec.ts)
+- [release notes](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-0.html)
--- src/3.0.spec.ts
+++ src/3.0.spec.ts
@@ -1,4 +1,65 @@
 describe('3.0', (): void => {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  it('', (): void => {});
+  describe('Rest parameters with tuple types', (): void => {
+    type TupleTest = [number, string, boolean];
+    function tailTuple(...rest: TupleTest): [string, boolean] {
+      return [rest[1], rest[2]];
+    }
+    it('spreads last args of a tuple to a sequence of discreet args of the tuple', (): void => {
+      const tuple: [number, string, boolean] = [42, 'hello', true];
+
+      const result = tailTuple(...tuple);
+
+      expect(result).toEqual(['hello', true]);
+    });
+  });
+  describe('Generic rest parameters', (): void => {
+    function bind<T, U extends any[], V>(f: (x: T, ...args: U) => V, x: T): (...args: U) => V {
+      return (...args) => f(x, ...args);
+    }
+
+    function f3(x: number, y: string, z: boolean): { x: number; y: string; z: boolean } {
+      return { x, y, z };
+    }
+
+    it('rest parameter is', (): void => {
+      const f2 = bind(f3, 42); // (y: string, z: boolean) => void
+      const f1 = bind(f2, 'hello'); // (z: boolean) => void
+      const f0 = bind(f1, true); // () => void
+
+      [f3(42, 'hello', true), f2('hello', true), f1(true), f0()].forEach((result) => {
+        expect(result).toEqual({
+          x: 42,
+          y: 'hello',
+          z: true,
+        });
+      });
+    });
+  });
+  describe('Optional elements in tuple types', (): void => {
+    it('allows optional entries at end of tuple types', (): void => {
+      let t: [number, string?, boolean?];
+      // length is union type 1 | 2 | 3
+
+      t = [42, 'hello', true];
+      expect(t.length).toBe(3);
+
+      t = [42, 'hello'];
+      expect(t.length).toBe(2);
+
+      t = [42];
+      expect(t.length).toBe(1);
+    });
+  });
+  describe('Rest elements in tuple types', (): void => {
+    function tuple<T extends any[]>(...args: T): T {
+      return args;
+    }
+    it('allows open ended tuples when last element of tuple is an array', (): void => {
+      const fib = [1, 1, 2, 3, 5];
+      const t1 = tuple('foo', 1, true); // [string, number, boolean]
+      expect(t1).toEqual(['foo', 1, true]);
+      const t2 = tuple('bar', ...fib); // [string, ...number[]]
+      expect(t2).toEqual(['bar', 1, 1, 2, 3, 5]);
+    });
+  });
 });
